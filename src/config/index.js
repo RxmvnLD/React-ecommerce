aa543c5c85a8c010eb95d839355e277563b8caa7
Roman Lemus <roman09071999@gmail.com>
2022-07-02 17:28:32 -0500

    Added product details in product page

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,5 @@
 export const baseURL = "https://backendnodejstzuzulcode.uw.r.appspot.com/api";
 export const stripePK =
   "pk_test_51KTd1dCxJ8HWxsAUvHdkJU90wXuUHO4qa4bF5dq3A7kCPWLAiaPnQ4bDpvBqIVMHPdABDwVMODmDff6jl8ok59OJ00SeHORvaW";
+export const defaultImage =
+  "https://thumbs.dreamstime.com/b/corrupted-file-document-outline-icon-corrupted-file-document-outline-icon-linear-style-sign-mobile-concept-web-design-bad-116231507.jpg";
